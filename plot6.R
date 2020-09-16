35c3e84d09cf0269be0534845375791c277b7c0f
yrigoyena22 <67293691+yrigoyena22@users.noreply.github.com>
2020-09-15 21:55:25 -0500

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -91,18 +91,18 @@ NIEvehicleBaltimoreTotEm<-aggregate(Emissions~year, NEIvehicleBaltimore, sum)
 ##with emissions from motor vehicle sources in Los Angeles County, 
 ##California (fips == "06037"|}fips == "06037"). Which city has seen 
 ##greater changes over time in motor vehicle emissions?
-NEIvehicleBalti<-subset(NEIvehicleSSC, fips == "24510")
-NEIvehicleBalti$city <- "Baltimore City"
+NEIvehicleBaltimore<-subset(NEIvehicleSSC, fips == "24510")
+NEIvehicleBaltimore$city <- "Baltimore City"
 NEIvehiclela<-subset(NEIvehicleSSC, fips == "06037")
 NEIvehiclela$city <- "Los Angeles County"
-NEIBothCity <- rbind(NEIvehicleBalti, NEIvehiclela)
+NEIBothCity <- rbind(NEIvehicleBaltimore, NEIvehiclela)
 ##Plotting the results
 ggplot(NEIBothCity, aes(x=year, y=Emissions, fill=city)) +
     geom_bar(aes(fill=year),stat="identity") +
     facet_grid(.~city) +
     guides(fill=FALSE) + theme_bw() +
     labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
-    labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
+    labs(title=expression("PM"[2.5]*" Motor Vehicle Emissions in Baltimore & LA areas, from 1999 to 2008"))
 ##To View the maximum change in  emission levels
 aggregateEmissions <- aggregate(Emissions~city+year, data=NEIBothCity, sum)
 aggregate(Emissions~city, data=aggregateEmissions, range)
@@ -110,5 +110,7 @@ aggregate(Emissions~city, data=aggregateEmissions, range)
 ##over time 
 dev.copy(png,"plot6.png", width=480, height=480)
 dev.off()
-##based on emissions information Los Angeles County has seen greater changes 
-##over time 
+##based on emissions information Los Angeles County shows high level of
+##emissions compared to the Baltimore city.Even though the Los Angeles county
+##at 2008 shows a reduction compared to 2005, that values is even more than 
+##10 times the emission observed in Baltimore area.  
